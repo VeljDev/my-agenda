c823cd9d435ae5436600f5915138dc05854edcff
VeljDev <veljkom9.vm@gmail.com>
2024-05-18 14:25:21 +0200

    Implemented homepage screen with drawer to switch between pages

---
+++ lib/events/event_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EventList extends StatelessWidget {
+  const EventList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('List of events');
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:my_agenda/calendar/calendar.dart';
+import 'package:my_agenda/events/event_list.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int _selectedIndex = 0;
+  static const List<Widget> _pages = <Widget>[
+    Calendar(),
+    EventList()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+      ),
+      body: Center(
+        child: _pages[_selectedIndex],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primaryContainer,
+              ),
+              child: const Text(
+                'Page selector',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.calendar_month),
+              title: const Text('Calendar'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(0);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.list),
+              title: const Text('Events'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                // Update the state of the app
+                _onItemTapped(1);
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
